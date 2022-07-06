3d4d8da46c70af5e63a8e609125ac7241b0cf997
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-07-05 20:10:22 -0500

    Use registerEvents because I can

--- src/main/kotlin/io/github/gaming32/qkdeathswap/DeathSwapMod.kt
+++ src/main/kotlin/io/github/gaming32/qkdeathswap/DeathSwapMod.kt
@@ -1,6 +1,6 @@
 package io.github.gaming32.qkdeathswap
 
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents.ALLOW_DEATH
 import net.minecraft.command.CommandException
 import net.minecraft.network.MessageType
 import net.minecraft.scoreboard.AbstractTeam.VisibilityRule
@@ -12,12 +12,12 @@ import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.loader.api.QuiltLoader
 import org.quiltmc.qkl.wrapper.minecraft.brigadier.literal
 import org.quiltmc.qkl.wrapper.minecraft.brigadier.register
-import org.quiltmc.qkl.wrapper.qsl.EventRegistration
 import org.quiltmc.qkl.wrapper.qsl.commands.onCommandRegistration
 import org.quiltmc.qkl.wrapper.qsl.lifecycle.onServerTickEnd
 import org.quiltmc.qkl.wrapper.qsl.networking.allPlayers
 import org.quiltmc.qkl.wrapper.qsl.networking.onPlayDisconnect
 import org.quiltmc.qkl.wrapper.qsl.networking.onPlayReady
+import org.quiltmc.qkl.wrapper.qsl.registerEvents
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -26,114 +26,116 @@ object DeathSwapMod : ModInitializer {
     @JvmField val LOGGER: Logger = LoggerFactory.getLogger("qkdeathswap")
 
     override fun onInitialize(mod: ModContainer) {
-        EventRegistration.onCommandRegistration { buildContext, environment ->
-            register("deathswap") {
-                requires { it.hasPermissionLevel(1) }
-                literal("start") {
-                    executes { ctx ->
-                        if (!QuiltLoader.isDevelopmentEnvironment() && ctx.source.server.allPlayers.size < 2) {
-                            throw CommandException(Text.literal("Cannot start a DeathSwap with less than 2 players."))
-                        }
-                        DeathSwapStateManager.begin(ctx.source.server)
-                        ctx.source.server.broadcast("Deathswap started!")
-                        1
-                    }
-                }
-                literal("stop") {
-                    executes { ctx ->
-                        DeathSwapStateManager.endGame(ctx.source.server)
-                        ctx.source.server.broadcast("Deathswap ended!")
-                        1
-                    }
-                }
-                literal("config") {
-                    DeathSwapConfig.CONFIG.values().forEach { option ->
-                        literal(option.key().toString()) {
-                            executes { ctx ->
-                                ctx.source.sendFeedback(Text.literal("${option.key()} -> ${option.value()}"), false)
-                                1
+        registerEvents {
+            onCommandRegistration { buildContext, environment ->
+                register("deathswap") {
+                    requires { it.hasPermissionLevel(1) }
+                    literal("start") {
+                        executes { ctx ->
+                            if (!QuiltLoader.isDevelopmentEnvironment() && ctx.source.server.allPlayers.size < 2) {
+                                throw CommandException(Text.literal("Cannot start a DeathSwap with less than 2 players."))
                             }
-                            val valueType = DeathSwapConfig.CONFIG_TYPES[option.key()]!!
-                            argument("value", valueType.first) {
-                                @Suppress("UNCHECKED_CAST")
+                            DeathSwapStateManager.begin(ctx.source.server)
+                            ctx.source.server.broadcast("Deathswap started!")
+                            1
+                        }
+                    }
+                    literal("stop") {
+                        executes { ctx ->
+                            DeathSwapStateManager.endGame(ctx.source.server)
+                            ctx.source.server.broadcast("Deathswap ended!")
+                            1
+                        }
+                    }
+                    literal("config") {
+                        DeathSwapConfig.CONFIG.values().forEach { option ->
+                            literal(option.key().toString()) {
                                 executes { ctx ->
-                                    val newValue = (valueType.second as (Any) -> Any)(ctx.getArgument("value", Any::class.java))
-                                    (option as TrackedValue<Any>).setValue(newValue, true)
-                                    ctx.source.sendFeedback(Text.literal("Successfully set ${option.key()} to $newValue"), true)
+                                    ctx.source.sendFeedback(Text.literal("${option.key()} -> ${option.value()}"), false)
+                                    1
+                                }
+                                val valueType = DeathSwapConfig.CONFIG_TYPES[option.key()]!!
+                                argument("value", valueType.first) {
+                                    @Suppress("UNCHECKED_CAST")
+                                    executes { ctx ->
+                                        val newValue = (valueType.second as (Any) -> Any)(ctx.getArgument("value", Any::class.java))
+                                        (option as TrackedValue<Any>).setValue(newValue, true)
+                                        ctx.source.sendFeedback(Text.literal("Successfully set ${option.key()} to $newValue"), true)
+                                        1
+                                    }
+                                }
+                            }
+                        }
+                        executes { ctx ->
+                            val result = Text.literal("Here are all the current config values:")
+                            DeathSwapConfig.CONFIG.values().forEach { option ->
+                                val name = option.key().toString()
+                                result.append("\n$name -> ${DeathSwapConfig.CONFIG.getValue(option.key()).value()}")
+                            }
+                            ctx.source.sendFeedback(result, false)
+                            1
+                        }
+                    }
+                    if (QuiltLoader.isDevelopmentEnvironment()) {
+                        literal("debug") {
+                            literal("swap_now") {
+                                executes { ctx ->
+                                    DeathSwapStateManager.timeToSwap = DeathSwapStateManager.timeSinceLastSwap
+                                    1
+                                }
+                            }
+                            literal("swap_at") {
+                                executes { ctx ->
+                                    ctx.source.sendFeedback(Text.literal(
+                                        "Will swap at: ${ticksToMinutesSeconds(DeathSwapStateManager.timeToSwap)}"
+                                    ), false)
                                     1
                                 }
                             }
                         }
                     }
-                    executes { ctx ->
-                        val result = Text.literal("Here are all the current config values:")
-                        DeathSwapConfig.CONFIG.values().forEach { option ->
-                            val name = option.key().toString()
-                            result.append("\n$name -> ${DeathSwapConfig.CONFIG.getValue(option.key()).value()}")
-                        }
-                        ctx.source.sendFeedback(result, false)
-                        1
-                    }
                 }
-                if (QuiltLoader.isDevelopmentEnvironment()) {
-                    literal("debug") {
-                        literal("swap_now") {
-                            executes { ctx ->
-                                DeathSwapStateManager.timeToSwap = DeathSwapStateManager.timeSinceLastSwap
-                                1
-                            }
-                        }
-                        literal("swap_at") {
-                            executes { ctx ->
-                                ctx.source.sendFeedback(Text.literal(
-                                    "Will swap at: ${ticksToMinutesSeconds(DeathSwapStateManager.timeToSwap)}"
-                                ), false)
-                                1
-                            }
+            }
+
+            ALLOW_DEATH.register { player, source, amount ->
+                if (!DeathSwapStateManager.hasBegun()) {
+                    return@register true
+                }
+                val shouldCancelDeathScreen = DeathSwapStateManager.livingPlayers.size > 2
+                if (shouldCancelDeathScreen) {
+                    // Copy-paste (and cleanup/Kotlin conversion) from ServerPlayerEntity#onDeath
+                    if (player.world.gameRules.getBoolean(GameRules.SHOW_DEATH_MESSAGES)) {
+                        val text = player.damageTracker.deathMessage
+                        val abstractTeam = player.scoreboardTeam
+                        if (abstractTeam == null || abstractTeam.deathMessageVisibilityRule == VisibilityRule.ALWAYS) {
+                            player.server.playerManager.broadcastSystemMessage(text, MessageType.SYSTEM)
+                        } else if (abstractTeam.deathMessageVisibilityRule == VisibilityRule.HIDE_FOR_OTHER_TEAMS) {
+                            player.server.playerManager.sendSystemMessageToTeam(player, text)
+                        } else if (abstractTeam.deathMessageVisibilityRule == VisibilityRule.HIDE_FOR_OWN_TEAM) {
+                            player.server.playerManager.sendSystemMessageToOtherTeams(player, text)
                         }
                     }
                 }
+                DeathSwapStateManager.removePlayer(player)
+                !shouldCancelDeathScreen
             }
-        }
 
-        ServerPlayerEvents.ALLOW_DEATH.register { player, source, amount ->
-            if (!DeathSwapStateManager.hasBegun()) {
-                return@register true
-            }
-            val shouldCancelDeathScreen = DeathSwapStateManager.livingPlayers.size > 2
-            if (shouldCancelDeathScreen) {
-                // Copy-paste (and cleanup/Kotlin conversion) from ServerPlayerEntity#onDeath
-                if (player.world.gameRules.getBoolean(GameRules.SHOW_DEATH_MESSAGES)) {
-                    val text = player.damageTracker.deathMessage
-                    val abstractTeam = player.scoreboardTeam
-                    if (abstractTeam == null || abstractTeam.deathMessageVisibilityRule == VisibilityRule.ALWAYS) {
-                        player.server.playerManager.broadcastSystemMessage(text, MessageType.SYSTEM)
-                    } else if (abstractTeam.deathMessageVisibilityRule == VisibilityRule.HIDE_FOR_OTHER_TEAMS) {
-                        player.server.playerManager.sendSystemMessageToTeam(player, text)
-                    } else if (abstractTeam.deathMessageVisibilityRule == VisibilityRule.HIDE_FOR_OWN_TEAM) {
-                        player.server.playerManager.sendSystemMessageToOtherTeams(player, text)
-                    }
+            onServerTickEnd {
+                if (DeathSwapStateManager.hasBegun()) {
+                    DeathSwapStateManager.tick(this)
                 }
             }
-            DeathSwapStateManager.removePlayer(player)
-            !shouldCancelDeathScreen
-        }
 
-        EventRegistration.onServerTickEnd {
-            if (DeathSwapStateManager.hasBegun()) {
-                DeathSwapStateManager.tick(this)
+            onPlayReady { packetSender, server ->
+                if (DeathSwapStateManager.hasBegun()) {
+                    DeathSwapStateManager.resetPlayer(player, gamemode = GameMode.SPECTATOR)
+                }
             }
-        }
 
-        EventRegistration.onPlayReady { packetSender, server ->
-            if (DeathSwapStateManager.hasBegun()) {
-                DeathSwapStateManager.resetPlayer(player, gamemode = GameMode.SPECTATOR)
-            }
-        }
-
-        EventRegistration.onPlayDisconnect {
-            if (DeathSwapStateManager.hasBegun()) {
-                DeathSwapStateManager.removePlayer(player, strikeLightning = false)
+            onPlayDisconnect {
+                if (DeathSwapStateManager.hasBegun()) {
+                    DeathSwapStateManager.removePlayer(player, strikeLightning = false)
+                }
             }
         }
 
